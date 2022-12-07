cbc492880413246f918d8c14fdbf00bc3b8b3791
g00343022 <g00343022@atu.ie>
2022-12-07 10:34:06 +0000

    Class and test class added and pom

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,4 +1,36 @@
 package ie.atu;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Employee {
+public class EmployeeInput {
+
+
+
+
+    public static void main(String[] args) {
+        ArrayList<Employee> listEmployee = new ArrayList<Employee>();
+        Scanner input = new Scanner((System.in));
+
+        System.out.println("Enter name: ");
+        String name = input.nextLine();
+        System.out.println("Enter PPS ID: ");
+        int pps = input.nextInt();
+        System.out.println("Enter phone number: ");
+        int phone = input.nextInt();
+        System.out.println("Enter Gender by number (1. Man, 2. Woman, 3. Non-Binary): ");
+        int gender = input.nextInt();
+        System.out.println("Employment type (Full-time or Part-Time): ");
+        String employment = input.nextLine();
+        System.out.println("Age: ");
+        int age = input.nextInt();
+
+        Employee object = new Employee(name, pps, phone, gender, employment, age);
+        listEmployee.add();
+
+    }
 }
+    public class Employee<listEmployee> {
+
+
+    }
--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -1,7 +1,42 @@
 package ie.atu;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
+    Employee myEmployee;
+
+    @BeforeEach
+    void setUp() {
+        myEmployee = new Employee();
+    }
+
+    @Test
+    void testName() {
+        assertEquals(5<22, myName.length()) ;
+    }
+    @Test
+    void testPPS() {
+    }
+    @Test
+    void testPhone() {
+    }
+    @Test
+    void testGender() {
+    }
+    @Test
+    void testType() {
+    }
+    @Test
+    void testAge() {
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
 }
